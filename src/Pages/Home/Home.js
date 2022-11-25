d4be54230c151751ca4dce2e4f0d1a4d7ebe31b5
devalamin <alamin.hasibmd2017@gmail.com>
2022-11-25 23:18:49 +0600

    reported

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import AdvertisedProducts from '../AdvertisedProducts/AdvertisedProducts';
 import Banner from '../Banner/Banner';
 import BookingModal from '../Shared/SingleCategory/BookingModal/BookingModal';
 import Categories from './Categories/Categories';
 
 const Home = () => {
+
+    const { adProducts } = useContext(AuthContext)
     return (
         <div>
             <Banner></Banner>
-            <AdvertisedProducts></AdvertisedProducts>
+            {adProducts && <AdvertisedProducts></AdvertisedProducts>}
             <Categories></Categories>
 
             {/* <BookingModal></BookingModal> */}
--- src/Pages/Shared/SingleCategory/ProductsCard.js
+++ src/Pages/Shared/SingleCategory/ProductsCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 import { MdOutlineVerified } from 'react-icons/md'
 
 const ProductsCard = ({ product, setBookingProduct }) => {
-    const { model, picture, condition, resale_price, original_price,
+    const { _id, model, picture, condition, resale_price, original_price,
         using_duration,
         location,
         seller_name,
@@ -11,9 +12,31 @@ const ProductsCard = ({ product, setBookingProduct }) => {
         time
     } = product;
 
+    const handleReportToAdmin = reportedProduct => {
+        fetch('http://localhost:5000/reported', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(reportedProduct)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success('Reported To Admin')
+                }
+                console.log(data);
+            })
+        // console.log(reportedProduct);
+
+    }
+
     return (
         <div>
+
             <div className="card lg:card-side bg-gradient-to-r from-white to-teal-900">
+
                 <figure><img className='sm:h-64 sm:w-96 object-cover' src={picture} alt="Album" /></figure>
                 <div className="card-body">
                     <h2 className="card-title sm:text-lg text-xs"><span className='text-teal text-xs sm:text-2xl sm:font-bold'>Model:</span>{model}</h2>
@@ -25,9 +48,9 @@ const ProductsCard = ({ product, setBookingProduct }) => {
                     <h2 className="card-title sm:text-lg text-xs"><span className='text-teal text-xs sm:text-2xl sm:font-bold'>Sell By:</span>  {seller_name} {seller_status ? <MdOutlineVerified className='text-amber-900 font-bold rounded-full' /> : <span><small className='text-xs text-lime-600'>New Seller</small></span>}</h2>
                     <h2 className="card-title sm:text-lg text-xs"><span className='text-teal text-xs sm:text-2xl sm:font-bold'>Posted On:</span>  {time?.slice(0, 16)}</h2>
 
-                    <div className="card-actions justify-end">
+                    <div className="card-actions justify-end items-center">
                         <label onClick={() => setBookingProduct(product)} htmlFor="product-modal" className="btn bg-amber-900">Buy Now</label>
-
+                        <button onClick={() => handleReportToAdmin(product)} className='btn btn-sm'>Report To Admin</button>
                     </div>
                 </div>
             </div>
